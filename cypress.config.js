b23299adf7c5c42f158bd763aa0ccc188b26fe50
unknown <aryanalphabin-design>
2025-11-28 15:51:52 +0530

    Added .yml for CI

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,9 @@
 const { defineConfig } = require("cypress");
+require("dotenv").config();
 
 module.exports = defineConfig({
+  projectId: process.env.CYPRESS_PROJECT_ID,
+
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
